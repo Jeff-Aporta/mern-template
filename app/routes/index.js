b18f27aa5925e37c9a0680af5bd879a4c2b498a6
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-10-26 17:41:22 -0500

    [dev-add] socket.io added

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,24 @@
 import apijson from "./api-json.js";
 import appcontrol from "./app-control.js";
 import authlogs from "./auth/logs.js";
+
 import path from "path";
 
+// Define __pathapp__ en tu archivo principal
+global.__pathapp__ = path.resolve(); // Esto establece la ruta absoluta en la raíz de tu proyecto
+
+// Función react
 function react({ app }) {
   app.get("/", (req, res) => {
-    res.sendFile(
-      path.join(global.__pathapp__, "client", "build", "index.html")
-    );
+    res.sendFile(path.join(global.__pathapp__, "client", "build", "index.html"));
   });
+  
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(global.__pathapp__, "client", "build", "index.html")
-    );
+    res.sendFile(path.join(global.__pathapp__, "client", "build", "index.html"));
   });
 }
 
+// Exporta tu función
 export default (packapp) => {
   apijson(packapp);
   appcontrol(packapp);
