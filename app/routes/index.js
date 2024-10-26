92839f61a6f740c6871d70fa9c58b323ebe3a528
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-10-26 17:51:53 -0500

    [dev-add] socket.io added

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,10 +4,6 @@ import authlogs from "./auth/logs.js";
 
 import path from "path";
 
-// Define __pathapp__ en tu archivo principal
-global.__pathapp__ = path.resolve(); // Esto establece la ruta absoluta en la raíz de tu proyecto
-
-// Función react
 function react({ app }) {
   app.get("/", (req, res) => {
     const file = path.join(global.__pathapp__, "client", "build", "index.html");
