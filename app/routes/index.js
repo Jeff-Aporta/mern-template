a981723fb5dd6974950bf434ffa4366866e1022b
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-10-26 22:30:00 -0500

    [test] index react

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,27 @@ import apijson from "./api-json.js";
 import appcontrol from "./app-control.js";
 import authlogs from "./auth/session.js";
 
+// Importar el módulo path
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+// Obtener el directorio del archivo actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const index = __dirname.replace(
+  join("app", "routes"),
+  join("client", "build", "index.html")
+);
+
+function react({ app }) {
+  app.get("*", (req, res, next) => {
+    res.sendFile(index);
+  });
+}
+
 export default (packapp) => {
   apijson(packapp);
   appcontrol(packapp);
   authlogs(packapp);
+  react(packapp);
 };
